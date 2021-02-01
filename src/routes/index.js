33000793787aa2c40002d1343e519366195e2268
franco.balich@gmail.com <franco.balich@gmail.com>
2021-02-01 01:42:13 -0300

    Se termino el proyecto

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const router = express.Router();
+
 // Se utilizo https://glb-packer.glitch.me/ para convertir los gltf a glb
 router.get("/",(req, res)=>{
     res.render("index", {
         title:"Catálogo de robots",
     products:[
-        {id:"1",title:"Auto Espacial", modelAndroid:"auto-espacial.glb", modelIos:"", scale : "1 1 1", portada:"auto-espacial.gif",
+        {id:"1",title:"Auto Especial", modelAndroid:"auto-espacial.glb", modelIos:"", scale : "0.1 0.1 0.1", portada:"auto-espacial.gif", direccion:"0",
         info:"Es un auto espacial diseñado para soportar una diversa cantidad de situaciones y terrenos." ,
     
     },
-        {id:"2",title:"Robot Espacial", modelAndroid:"robot_espacial.glb", modelIos:"", scale : "0.9 0.9 0.9", portada:"robot-espacial.gif",
+        {id:"2",title:"Robot Espacial", modelAndroid:"robot_espacial.glb", modelIos:"", scale : "0.9 0.9 0.9", portada:"robot-espacial.gif",direccion:"3",
         info:"Es un robot diseñado para la exploración espacial y posee paneles solares en su espalda para poder cargar sus baterías." ,
     
     },
-        {id:"3",title:"Robot con brazos", modelAndroid:"robot-con-brazos.glb", modelIos:"", scale : "0.2 0.2 0.2", portada:"robot-con-brazos.gif",
+        {id:"3",title:"Robot con brazos", modelAndroid:"robot-con-brazos.glb", modelIos:"", scale : "0.2 0.2 0.2", portada:"robot-con-brazos.gif",direccion:"3",
         info:"Este robot se especializa en mover objetos de un lugar a otro con sus brazos mecánicos." ,
     },
-        {id:"4",title:"Robot Camara", modelAndroid:"robot-camara.glb", modelIos:"", scale : "1 1 1", portada:"robot-camara.gif",
+        {id:"4",title:"Robot Camara", modelAndroid:"robot-camara.glb", modelIos:"", scale : "1.3 1.3 1.3", portada:"robot-camara.gif",direccion:"2",
         info:"Es un robot que se caracteriza por la gran cámara que posee al frente de su estructura."
     }
     ]});   
