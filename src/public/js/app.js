644249164840f7900418b913349782ec8188b2c0
franco.balich@gmail.com <franco.balich@gmail.com>
2021-01-31 23:52:11 -0300

    Primer prototipo funcional

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,8 @@
 var buttons = document.querySelectorAll('button');
-
 function ButtonC(msg){
     alert(msg);
 }
+
 /*
 buttons.forEach((item)=>{
     item.addEventListener('click', ()=>{
@@ -10,13 +10,4 @@ buttons.forEach((item)=>{
     });
 });
 */
-document.querySelector("#model-viewer").addEventListener('ar-status', (event) => {
-    if(event.detail.status === 'not-session-started'){
-        var boton = document.getElementById("btnControl");
-        boton.removeClass('hide');
-        
-    }
-    else{
-        boton.addClass('hide');
-    }
-  });
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,37 +3,22 @@ const router = express.Router();
 // Se utilizo https://glb-packer.glitch.me/ para convertir los gltf a glb
 router.get("/",(req, res)=>{
     res.render("index", {
-        title:"Catalogo de muebles",
+        title:"Catálogo de robots",
     products:[
-        {id:"1",title:"Robot Espacial", modelAndroid:"auto-espacial.glb", modelIos:"", 
-        info:"Lorem ipsum dolor sit amet consectetur adipiscing elit, class sapien cras justo venenatis pretium convallis neque, taciti dictum tellus leo eu parturient. " ,
+        {id:"1",title:"Auto Espacial", modelAndroid:"auto-espacial.glb", modelIos:"", scale : "0.1 0.1 0.1", portada:"auto-espacial.gif",
+        info:"Es un auto espacial diseñado para soportar una diversa cantidad de situaciones y terrenos." ,
     
     },
-        {id:"2",title:"Perro robot", modelAndroid:"perro-robot.glb", modelIos:"", 
-        info:"Lorem ipsum dolor sit amet consectetur adipiscing elit, class sapien cras justo venenatis pretium convallis neque, taciti dictum tellus leo eu parturient. " ,
-
-    },
-        {id:"3",title:"Auto Espacial", modelAndroid:"robot_espacial.glb", modelIos:"", 
-        info:"Lorem2 ipsum dolor sit amet consectetur adipiscing elit, class sapien cras justo venenatis pretium convallis neque, taciti dictum tellus leo eu parturient. " ,
+        {id:"2",title:"Robot Espacial", modelAndroid:"robot_espacial.glb", modelIos:"", scale : "0.9 0.9 0.9", portada:"robot-espacial.gif",
+        info:"Es un robot diseñado para la exploración espacial y posee paneles solares en su espalda para poder cargar sus baterías." ,
     
     },
-        {id:"4",title:"Robot Camara", modelAndroid:"robot-camara.glb", modelIos:"", 
-        info:"Lorem2 ipsum dolor sit amet consectetur adipiscing elit, class sapien cras justo venenatis pretium convallis neque, taciti dictum tellus leo eu parturient. " ,
-
+        {id:"3",title:"Robot con brazos", modelAndroid:"robot-con-brazos.glb", modelIos:"", scale : "0.2 0.2 0.2", portada:"robot-con-brazos.gif",
+        info:"Este robot se especializa en mover objetos de un lugar a otro con sus brazos mecánicos." ,
     },
-        {id:"5",title:"Robot con Brazos", modelAndroid:"robot-con-brazos.glb", modelIos:"", 
-        info:"Lorem2 ipsum dolor sit amet consectetur adipiscing elit, class sapien cras justo venenatis pretium convallis neque, taciti dictum tellus leo eu parturient. " ,
-
-    },
-    ]});
+        {id:"4",title:"Robot Camara", modelAndroid:"robot-camara.glb", modelIos:"", scale : "0.45 0.45 0.45", portada:"robot-camara.gif",
+        info:"Es un robot que se caracteriza por la gran cámara que posee al frente de su estructura."
+    }
+    ]});   
 });
-
-router.get("/control",(req, res)=>{
-    res.render("control", {title:"Seccion de contacto",
-    products:[
-        {id:"1",title:"mesa 1",model:"modelo.gltf"},
-        {id:"2",title:"mesa 2",model:"modelo2.gltf"} 
-    ]});
-});
-
 module.exports=router;
